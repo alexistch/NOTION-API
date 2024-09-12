94986ade60555e4664392877b3df1d62d0d3496a
unknown <alexis.tchoulev@efrei.net>
2024-09-12 18:21:56 +0200

    version avec photo et CV

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
- // identifier le btn
- const btn = document.getElementById("btn");
 
- // définir une fonction
- function handleClick(e) {
-     e.target.innerHTML = "bonjour";
- }
+ document.addEventListener('DOMContentLoaded', function () {
+    const section = document.querySelector('.main_section');
+    let positionX = 50; // Position de départ en X
+    let positionY = 50; // Position de départ en Y
+    let speedX = 0.6; // Vitesse du déplacement en X
+    let speedY = 0.6; // Vitesse du déplacement en Y
 
- // ajouter un événement click
- btn.addEventListener("click", handleClick);
+    function animateBackground() {
+        positionX += speedX;
+        positionY += speedY;
 
- // log pour tester
- console.log("test ...", btn);
+        if (positionX > 100 || positionX < 0) {
+            speedX = -speedX; // Inverser la direction en X
+        }
+        if (positionY > 100 || positionY < 0) {
+            speedY = -speedY; // Inverser la direction en Y
+        }
+
+        section.style.backgroundPosition = `${positionX}% ${positionY}%`;
+
+        requestAnimationFrame(animateBackground);
+    }
+
+    animateBackground();
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const section = document.querySelector('.second_section');
+    let positionX = 50; // Position de départ en X
+    let positionY = 50; // Position de départ en Y
+    let speedX = 0.6; // Vitesse du déplacement en X
+    let speedY = 0.6; // Vitesse du déplacement en Y
+
+    function animateBackground() {
+        positionX += speedX;
+        positionY += speedY;
+
+        if (positionX > 100 || positionX < 0) {
+            speedX = -speedX; // Inverser la direction en X
+        }
+        if (positionY > 100 || positionY < 0) {
+            speedY = -speedY; // Inverser la direction en Y
+        }
+
+        section.style.backgroundPosition = `${positionX}% ${positionY}%`;
+
+        requestAnimationFrame(animateBackground);
+    }
+
+    animateBackground();
+});
